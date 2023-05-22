5cd4ef3399da692e5468f9facd9c9835db973123
Kyle Truong <Kyle.Truong@dyson.com>
2023-05-22 16:25:18 +0800

    Initial Commit

---
+++ ScrollViewStuff/ScrollViewStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewStuffApp.swift
+//  ScrollViewStuff
+//
+//  Created by Truong Kyle on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
