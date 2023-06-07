829ec45e0895179325ef3aad4472573f2f15cddb
Kyle Truong <Kyle.Truong@dyson.com>
2023-06-07 14:07:44 +0800

    Add scroll view

--- ScrollViewStuff/ContentView.swift
+++ ScrollViewStuff/ContentView.swift
@@ -7,20 +7,147 @@
 
 import SwiftUI
 
+class Model: ObservableObject {
+    @Published var tabBarYOffset: CGFloat = .zero
+}
+
 struct ContentView: View {
+    
+    @StateObject private var model = Model()
+    @State private var selection: Int = .zero
+    @State private var offset: CGPoint = .zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        ZStack(alignment: .top) {
+//            // Hero image
+//            Image(systemName: "globe")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(height: 300)
+//
+//            // Tab bar
+//            HStack(spacing: 20) {
+//                Button("Tab 1") {
+//                    selection = 0
+//                }
+//
+//                Button("Tab 2") {
+//                    selection = 1
+//                }
+//            }
+//            .frame(maxWidth: .infinity)
+//            .frame(height: 50)
+//            .padding(.horizontal)
+//            .background(.thinMaterial)
+//            .offset(y: max(250-model.tabBarYOffset, .zero))
+//            .zIndex(1)
+//
+//            // Tab content
+//            TabView(selection: $selection) {
+//                TabContentVScrollView {
+//                    ForEach(0..<100) { idx in
+//                        Text("Tab \(selection + 1) - Line at \(idx)")
+//                            .frame(maxWidth: .infinity)
+//                    }
+//
+//                }
+//                .tag(0)
+//
+//                TabContentVScrollView {
+//                    ForEach(0..<100) { idx in
+//                        Text("Tab \(selection + 1) - Line at \(idx)")
+//                            .frame(maxWidth: .infinity)
+//                    }
+//                }
+//                .tag(1)
+//            }
+//            .environmentObject(model)
+//            .tabViewStyle(.page(indexDisplayMode: .never))
+//            .edgesIgnoringSafeArea(.bottom)
+//        }
+        VScrollView(offset: $offset) {
+            VStack(spacing: .zero) {
+                // Transparency
+                Color.clear
+                    .frame(height: 300)
+                // Main content
+                ForEach(0..<50) { idx in
+                    Text("Tab \(selection + 1) - Line at \(idx)")
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .frame(maxHeight: .infinity)
+            .background(Color.green)
         }
-        .padding()
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+// MARK: - Tab Content View
+
+// UIKit ScrollView
+struct TabContentVScrollView<Content: View>: View {
+    
+    @EnvironmentObject var model: Model
+    @ViewBuilder let content: Content
+    @State private var offset: CGPoint = .zero
+    
+    var body: some View {
+        VScrollView(offset: $offset) {
+            VStack(spacing: .zero) {
+                // Transparency
+                Color.clear
+                    .frame(height: 300)
+                // Main content
+                content
+            }
+            .background(Color.green)
+            .edgesIgnoringSafeArea(.bottom)
+        }
+        .onChange(of: offset.y) { newValue in
+            model.tabBarYOffset = newValue
+        }
+        .onAppear {
+            offset.y = model.tabBarYOffset
+            print("--> onAppear \(offset.y)")
+        }
+        .background(Color.red)
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+// Native SwiftUI Scroll View
+struct TabContentScrollView<Content: View>: View {
+    
+    @EnvironmentObject var model: Model
+    @ViewBuilder let content: Content
+    @State private var offset: CGPoint = .zero
+    
+    var body: some View {
+        OffsetObservingScrollView(offset: $offset) {
+            VStack(spacing: .zero) {
+                // Transparency
+                Color.clear
+                    .frame(height: 300)
+                // Main content
+                content
+            }
+        }
+        .onChange(of: offset.y) { newValue in
+            model.tabBarYOffset = newValue
+        }
+        .onAppear {
+            offset.y = model.tabBarYOffset
+            print("--> onAppear \(offset.y)")
+        }
+        .background(Color.red)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
---
+++ ScrollViewStuff/UIScrollViewWrapper/VScrollView.swift
@@ -0,0 +1,76 @@
+//
+//  UIScrollViewWrapper.swift
+//  ScrollViewStuff
+//
+//  Created by Truong Kyle on 22/05/2023.
+//
+
+import SwiftUI
+
+struct VScrollView<Content: View>: UIViewRepresentable {
+    
+    @Binding var offset: CGPoint
+    
+    let content: Content
+    
+    init(
+        offset: Binding<CGPoint>,
+        @ViewBuilder content: () -> Content)
+    {
+        _offset = offset
+        self.content = content()
+    }
+    
+    func makeCoordinator() -> ScrollViewCoordinator {
+        ScrollViewCoordinator(offset: $offset)
+    }
+    
+    func makeUIView(context: Context) -> UIScrollView {
+        let coordinator = context.coordinator
+        
+        let scrollView = UIScrollView()
+        scrollView.delegate = coordinator
+        scrollView.contentInset.bottom = .zero
+        
+        let hostingController = UIHostingController(rootView: content)
+        let hostedView = hostingController.view!
+        hostedView.backgroundColor = .clear
+        scrollView.addSubview(hostedView)
+        
+        hostedView.translatesAutoresizingMaskIntoConstraints = false
+        hostedView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor).isActive = true
+        hostedView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor).isActive = true
+        hostedView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor).isActive = true
+        hostedView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor).isActive = true
+        
+        // this is important for scrolling
+        hostedView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
+        
+        content.environment(\., <#T##value: V##V#>)
+        return scrollView
+    }
+    
+    func updateUIView(_ scrollView: UIScrollView, context: Context) {
+//        print("--> updateUIView \(offset.y)")
+//        scrollView.layoutIfNeeded()
+        scrollView.contentOffset.y = offset.y
+    }
+}
+
+class ScrollViewCoordinator : NSObject, UIScrollViewDelegate {
+    
+    @Binding var offset: CGPoint
+    
+    init(offset: Binding<CGPoint>) {
+        _offset = offset
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        DispatchQueue.main.async {
+//            print("--> scrollDidScroll \(scrollView.contentOffset.y)")
+            self.offset = scrollView.contentOffset
+//        }
+    }
+}
